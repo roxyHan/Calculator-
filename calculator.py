39b588e7882d671f319874f5dd17cd30b9a17937
Hanifa Barry <40450542+roxyHan@users.noreply.github.com>
2019-11-06 06:46:35 -0800

    Design of calculator program
    *--> Listed the main steps to develop a calculator program
    *--> Thinking of python for the programming language

---
+++ calculator.py
@@ -0,0 +1,17 @@
+# Design of the program
+# Calculator program
+
+def main():
+  # Get user input for the expression to calculate
+  
+  # Evaluate the expression as a string
+  
+  # Use infix concept for correct outputs
+  
+  # Can use helper functions to recude code redundancy
+  
+  # Ideas of helper functions would be: is_parity_of_parentheses, order_of_operations, compute_operation, display_result
+  
+ 
+if __name__ == "__main__":
+  main()  
